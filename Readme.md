ffa1f07c3f1655c6e4d007c2800b21326e1faa77
Khoa Nguyen <khoanguyen@Khoas-MacBook-Pro-2.local>
2018-08-28 14:03:32 -0700

    created storepicker component

---
+++ Readme.md
@@ -0,0 +1,5 @@
+clone package to your local machine
+** In the root project where package.json file is run this command **
+npm install
+** To start the project ** 
+npm start
---
+++ src/components/StorePicker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class StorePicker extends React.Component {
+  render(){
+    return(
+      <form className="store-selector">
+        <p>Please Enter A Store</p>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Visit Store →</button>
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
