d510ff9cf678b8e1c27aab3069d1d3356ff6162a
Khoa Nguyen <khoanguyen@Khoas-MacBook-Pro-2.local>
2018-08-29 15:00:21 -0700

    displaying fish

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order' ;
 import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
+
 
 class App extends React.Component {
   constructor(){
     super();
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     //initial state
     this.state = {
         fishes: {},
@@ -23,14 +27,26 @@ class App extends React.Component {
     //set state for the new fish only
     this.setState({fishes: fishes});
   }
+  loadSamples(){
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
   render() {
     return (
       <div className='catch-of-the-day'>
          <div className='menu'>
            <Header tagline='Fresh seafood market' />
+           <ul className="list-of-fishes">
+              {
+                Object
+                  .keys(this.state.fishes)
+                  .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+              }
+           </ul>
          </div>
          <Order />
-         <Inventory addFish={this.addFish}/> 
+         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/> 
       </div> 
     );
   }
